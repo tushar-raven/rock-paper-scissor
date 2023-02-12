c6b87e6f4d34cfa49a14b4b6edc7a0f583b3af46
tusharravenclaw <tushar68053@gmail.com>
2023-02-12 21:00:16 +0530

    Add resetButton() and declareWinner()

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-//Using buttons to select player
-const buttons = document.querySelectorAll(".button");
-
+let playerWeapon;
 let playerScore = 0;
 let computerScore = 0;
+
+const playerChoice = document.querySelector(".player-choice");
+const ComputerChoice = document.querySelector(".computer-choice");
+const roundWinner = document.querySelector(".round-winner");
+const gameWinner = document.querySelector(".announce");
+const playScore = document.querySelector(".player-score");
+const compScore = document.querySelector(".computer-score");
+const resetDiv = document.querySelector(".reset-button");
+
+const buttons = document.querySelectorAll(".button");
 
 buttons.forEach(button =>
     button.addEventListener("click", playerSelect)
@@ -11,26 +19,53 @@ buttons.forEach(button =>
 function playerSelect(e) {
     playerWeapon = e.target.id;
     game()
-    
+    if(playerScore == 5|| computerScore == 5){
+    declareWinner()
+    }
 }
 
-let playerWeapon;
+function declareWinner() {
+    if (playerScore == 5){
+        gameWinner.textContent = "You Win The Game!!!"
+        } else if (computerScore == 5){
+        gameWinner.textContent = "Computer Win The Game!!!"
+    }
+
+    playerScore = 0;
+    computerScore = 0;
+    playerChoice.remove();
+    ComputerChoice.remove();
+    roundWinner.remove();
+    
+    buttons.forEach(button =>
+        button.remove()
+    )
+    
+    const resetButton = document.createElement("button")
+    const resetText = document.createTextNode("Reset Game")
+    resetDiv.append(resetButton)
+    resetButton.appendChild(resetText)
+    resetButton.addEventListener('click', resetGame)
+}
+
+function resetGame() {
+    location.reload()
+}
 
 function game() {
 
     const playerSelection = playerWeapon;
-    const playerChoice = document.querySelector(".player-choice");
     playerChoice.textContent = playerSelection;
 
     const computerSelection = getComputerChoice();
-    const ComputerChoice = document.querySelector(".computer-choice")
     ComputerChoice.textContent = computerSelection;
 
-    const roundWinner = document.querySelector(".round-winner")
     roundWinner.textContent = playRound(playerSelection, computerSelection);
 
-    console.log(playerScore);
-    console.log(computerScore);
+    playScore.textContent = playerScore;
+
+    compScore.textContent = computerScore;
+
 }
 
 function getComputerChoice() {
